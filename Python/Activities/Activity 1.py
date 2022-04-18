17ab76eb213f041fd6b9eee8aeb009de4a4131d0
Sundar Rajamani <103921339+sundar819@users.noreply.github.com>
2022-04-18 14:23:22 +0530

    Add files via upload

---
+++ Python/Activities/Activity 1.py
@@ -0,0 +1,10 @@
+import sunau
+
+
+Name = input("enter your name")
+Age = input("enter you age")
+print(Name,Age)
+cage=100-int(Age)
+cyear=2022;
+turn100age=cage+cyear
+print(Name +" will reach 100 on  " +str(turn100age))
